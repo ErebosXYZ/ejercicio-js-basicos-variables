8e0666b5fb6d87de263c6bd6461b871492ee8ab0
ErebosXYZ <robertmaynou@gmail.com>
2025-03-05 20:53:46 +0100

    exercici 3 resolt

--- ex3.js
+++ ex3.js
@@ -5,10 +5,10 @@
 let nombre = "Mad";
 let apellido = "Max";
 
-let presentacion = "";
+let presentacion = "Me llamo Mad Max";
 
 /**
- * Pregunta 1: Modifica la línea 8. ¿Qué valor tengo que asignar a 'presenetacion' para que me muestre pantalla:
+ * Pregunta 1: Modifica la línea 8. ¿Qué valor tengo que asignar a 'presentacion' para que me muestre pantalla:
  *   * "Me llamo Mad Max"
  *
  * 
@@ -20,5 +20,7 @@ console.log("Pregunta 1:", presentacion);
 /**
  * Pregunta 2: Cambia el valor de las variables adecuadamente para que, en vez de "Me llamo Mad Max", escribe "Me llamo {tu nombre} {tu apellido}"
  */
+
+presentacion = "Me llamo Robert Maynou";
 
 console.log("Pregunta 2:", presentacion);
